1eb8b9d247fc610c37c300a01d97159ac4875aab
TheLukCraft <luk1997r@o2.pl>
2024-08-09 23:41:17 +0200

    using angular bootstrap components

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,11 +1,12 @@
 import { AccountService } from './../_services/account.service';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, BsDropdownModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
@@ -25,6 +26,6 @@ export class NavComponent {
     })
   }
   logout() {
-    this.loggedIn
+    this.loggedIn = false;
   }
 }
