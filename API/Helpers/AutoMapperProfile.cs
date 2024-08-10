3c3f0e8bab881eca92b6a9b4d900b27d0b2d777b
TheLukCraft <luk1997r@o2.pl>
2024-08-10 22:17:13 +0200

    Configuring AutoMapper

--- API/Helpers/AutoMapperProfile.cs
+++ API/Helpers/AutoMapperProfile.cs
@@ -9,7 +9,9 @@ public class AutoMapperProfile : Profile
 {
     public AutoMapperProfile()
     {
-        CreateMap<AppUser, MemberDto>();
+        CreateMap<AppUser, MemberDto>()
+        .ForMember(dest => dest.PhotoUrl, opt => 
+            opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain)!.Url));
         CreateMap<Photo, PhotoDto>();
     }
 }
