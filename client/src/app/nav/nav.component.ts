1f6be6205ec429fc71369d9752cd410d55b93fe6
TheLukCraft <luk1997r@o2.pl>
2024-08-10 04:12:44 +0200

    Adding a toast service for notifications

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -4,6 +4,7 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -15,6 +16,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 export class NavComponent {
   accountService = inject(AccountService);
   private router = inject(Router);
+  private toastr = inject(ToastrService)
   model: any = {};
 
   login() {
@@ -23,7 +25,7 @@ export class NavComponent {
         console.log(response);
         void this.router.navigateByUrl('/members');
       },
-      error: error => console.log(error)
+      error: error => this.toastr.error(error.error)
 
     })
   }
