0a4df744653c987bb46d2360ef6e3f19580be439
TheLukCraft <luk1997r@o2.pl>
2024-08-10 22:07:49 +0200

    Adding AutoMapper

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -19,7 +19,7 @@ public class AppUser
     public required string Interests { get; set; }
     public required string City { get; set; }
     public required string Country { get; set; }
-    public List<Photo> Photos { get; set; }
+    public List<Photo>? Photos { get; set; }
 
     public int GetAge() 
     {
---
+++ API/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+using System;
+using API.DTOs;
+using API.Entities;
+using AutoMapper;
+
+namespace API.Helpers;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<AppUser, MemberDto>();
+        CreateMap<Photo, PhotoDto>();
+    }
+}
