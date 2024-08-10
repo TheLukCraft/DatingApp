8ee0f74a9317f42f31fb942d67a80418182350f4
TheLukCraft <luk1997r@o2.pl>
2024-08-10 04:02:53 +0200

    adding the nav links and adding nav links

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,23 +1,27 @@
+import { routes } from './../app.routes';
 import { AccountService } from './../_services/account.service';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router);
   model: any = {};
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response =>  {
         console.log(response);
+        void this.router.navigateByUrl('/members');
       },
       error: error => console.log(error)
 
@@ -25,5 +29,6 @@ export class NavComponent {
   }
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
