49438b348a96b5327118bf4ffb0b8bf4e0f2bb34
TheLukCraft <luk1997r@o2.pl>
2024-08-29 14:54:23 +0200

    Restoring caching for member detailed

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -5,6 +5,7 @@ import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
 import { Photo } from '../_models/photo';
 import { PaginatedResult } from '../_models/pagination';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -51,8 +52,11 @@ export class MembersService {
   }
 
   getMember(username: string) {
-    // const member = this.members().find(x => x.username === username);
-    // if(member !== undefined) return of(member);
+    const member:Member = [...this.memberCache.values()]
+    .reduce((arr, elem) => arr.concat(elem.body), [])
+    .find((m: Member) => m.username === username);
+
+    if(member) return of(member);
 
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
