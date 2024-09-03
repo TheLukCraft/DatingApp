bb74701121d70c6a59673eaceded369bbb46504b
TheLukCraft <luk1997r@o2.pl>
2024-09-04 01:11:25 +0200

    Getting the message thread for 2 users

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -52,4 +52,12 @@ IUserRepository userRepository, IMapper mapper) : BaseApiController
 
         return messages;
     }
+
+    [HttpGet("thread/{username}")]
+    public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessageThread(string username)
+    {
+        var currentUsername = User.GetUsername();
+
+        return Ok(await messageRepository.GetMessageThread(currentUsername, username));
+    }
 }
