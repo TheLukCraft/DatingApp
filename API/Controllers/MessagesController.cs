7817ccd43a752a29f9d4cfa7521f7e59c43f0796
TheLukCraft <luk1997r@o2.pl>
2024-09-03 22:35:34 +0200

    Getting the messages from the Repo

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
@@ -39,5 +40,16 @@ IUserRepository userRepository, IMapper mapper) : BaseApiController
             return Ok(mapper.Map<MessageDto>(message));
         
         return BadRequest("Failed to send message");
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser([FromQuery] MessageParams messageParams)
+    {
+        messageParams.Username = User.GetUsername();
+
+        var messages = await messageRepository.GetMessagesForUser(messageParams);
+
+        Response.AddPaginationHeader(messages);
+
+        return messages;
     }
 }
